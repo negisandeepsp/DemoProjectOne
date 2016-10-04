7ab4c7904b39bc5255102fe9a1317db3f6cef5f4
splash-math <sandeep.negi@splashmath.com>
2016-10-04 13:24:30 +0530

    first change

--- src/myPackage/Googly.java
+++ src/myPackage/Googly.java
@@ -3,7 +3,8 @@ package myPackage;
 public class Googly {
 	public static void main(String args[])
 	{
-		System.out.println("Hellooo");
+		System.out.println("Hellooo java");
+		System.out.println("Doing good work");
 	}
 
 }
